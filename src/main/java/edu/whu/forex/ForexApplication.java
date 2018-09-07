4443bf38dd3da4b4714834d41ae8eeeb49dc1ab0
lyricly <934991794@qq.com>
2018-09-07 16:42:58 +0800

    init

---
+++ src/main/java/edu/whu/forex/ForexApplication.java
@@ -0,0 +1,12 @@
+package edu.whu.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexApplication.class, args);
+    }
+}
